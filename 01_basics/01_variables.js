9c8d08faf93fade52c3b71641fcf6515957efbdc
Prabhat30720 <67800273+Prabhat30720@users.noreply.github.com>
2025-02-05 17:48:41 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553;
+let accountEmail = "prabhatjaiswar30720@gmail.com";
+var accountPassword = "12345678";
+accountCity = "Mumbai";
+let accountState;
+
+// accountId = 123456;
+
+console.log(accountId);
+
+accountEmail = "prabhatjaiswar007@gmail.com";
+accountPassword = "Prabhat@1234";
+accountCity = "Bengaluru";
+
+console.table([accountId ,accountEmail, accountPassword, accountCity, accountState]);
