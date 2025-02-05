c5fc50f918ad20a0c370a88c13f706c2210f1ca0
Prabhat30720 <67800273+Prabhat30720@users.noreply.github.com>
2025-02-05 19:24:17 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/*
+Pre-mitive data types in javascript:
+
+1) number
+2) string
+3) boolean
+4) bigInt
+5) null
+6) undefined
+7) symbol => use for checking uniqeness
+*/
+console.log(typeof null);       // object
+console.log(typeof undefined);  // undefined 
