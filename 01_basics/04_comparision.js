d5b413c29e4892d8a7e92471df2ecb12d87ec011
Prabhat30720 <67800273+Prabhat30720@users.noreply.github.com>
2025-02-06 19:02:47 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,17 @@
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined < 0);
+
+
+// === strict check --> It check the data types along with the values
+
+console.log("strict checking ===");
+console.log("2" == 2);      // true
+console.log("2" === 2);     // false
