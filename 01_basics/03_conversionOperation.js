6a37bb075c3101e476338099e3b578d615251b51
Prabhat30720 <67800273+Prabhat30720@users.noreply.github.com>
2025-02-06 18:34:45 +0000

    Arithmetic Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,3 +26,23 @@ const randomNumber = 33;
 const stringNumber = String(randomNumber);
 console.log(typeof stringNumber);
 console.log(stringNumber);
+
+// ***************************************************  Operations  *********************************************************
+
+const value = 3;
+const negValue = -value;
+console.log(negValue);
+
+const str1 = "Hello";
+const str2 = " Prabhat";
+
+console.log(str1 + str2);
+console.log(1 + 2);
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(true);
+
+let gameCounter = 100;
+gameCounter++;
+
+console.log(gameCounter);
