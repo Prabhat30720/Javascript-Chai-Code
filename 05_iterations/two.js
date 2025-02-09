e16810cb0e6b8ce8a9e3885cdad4de136ca21896
Prabhat30720 <67800273+Prabhat30720@users.noreply.github.com>
2025-02-09 16:33:23 +0000

    do-while

---
+++ 05_iterations/two.js
@@ -0,0 +1,28 @@
+// while() loop
+
+let index = 0;
+
+while (index <= 10) {
+    console.log(index);
+    index = index + 2;
+}
+
+
+const superHeros = ["superman", "batman", "thor", "hulk", "spiderman"];
+
+let arrayIncrement = 0;
+
+while( arrayIncrement < superHeros.length){
+    console.log(superHeros[arrayIncrement]);
+    arrayIncrement++;
+}
+
+
+// do-while loop
+
+let score = 11;
+
+do {
+    console.log(score);
+    score++;
+} while (score <= 10);
