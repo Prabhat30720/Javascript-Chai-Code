0779185c9a1785b2cffa432b61e42490b91156ff
Prabhat30720 <67800273+Prabhat30720@users.noreply.github.com>
2025-02-17 15:45:52 +0000

    events in js

--- 08_events/eventbasics.js
+++ 08_events/eventbasics.js
@@ -10,17 +10,21 @@
 
 document.getElementById("owl").addEventListener("click", function(event){
     console.log("Owl Clicked");
+
+    // When we don't want the reaction of outer element to happen in case of event bubling we can use stopPropagation() methos 
+
+    event.stopPropagation();
 }, false); */
 
 // Event Propogation: event capturing ---> event occurs from outer HTML elements to inner HTML elements
 
-document.getElementById("images").addEventListener("click", function(event){
+/* document.getElementById("images").addEventListener("click", function(event){
     console.log("Clicked inside the ul");
 }, true);
 
 document.getElementById("owl").addEventListener("click", function(event){
     console.log("Owl Clicked");
-}, true);
+}, true); */
 
 /* 
 
@@ -28,3 +32,25 @@ Research topics: type, timeStamp, defaultPrevented, target, toElement, srcElemen
 altkey, ctrlkey, shiftkey, keyCode
 
 */
+
+/* document.getElementById("google").addEventListener("click", function(event){
+    // preventDefault(); prevents the default behaviour
+    console.log("Google clicked!");
+    event.preventDefault();
+    event.stopPropagation();
+}, false); */
+
+
+// Remove the image from DOM on which it is clicked
+
+document.querySelector("#images").addEventListener("click", function(event){
+    console.log(event.target.tagName);
+
+    if (event.target.tagName === "IMG") {        
+        const removeIt = event.target.parentNode;
+        console.log(event.target.id);
+        removeIt.remove();
+    }
+
+    // removeIt.parentNode.removeChild(removeIt);
+});
