b1fef16de5802cc9b90fce0ada0d1b497fa5bf89
Prabhat30720 <67800273+Prabhat30720@users.noreply.github.com>
2025-03-06 11:21:28 +0000

    prototype

---
+++ 10_classes_and_oop/prototype.js
@@ -0,0 +1,11 @@
+let name = "Prabhat       ";
+
+console.log(name.length);
+
+let heros = ["Ironman", "Spiderman", "Thor"];
+
+let herosPower = {
+    Ironman: "AI Suit",
+    Spiderman: "Sling",
+    Thor: "Hammer",
+};
