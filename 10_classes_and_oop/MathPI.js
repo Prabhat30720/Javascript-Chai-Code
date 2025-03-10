fa42d49e4523d18b3f05d3f00bcede74d1f50f61
Prabhat30720 <67800273+Prabhat30720@users.noreply.github.com>
2025-03-10 10:28:24 +0000

    property descriptor

---
+++ 10_classes_and_oop/MathPI.js
@@ -0,0 +1,41 @@
+console.log(Math.PI);
+
+// Trying to change the value of PI but it won't change
+
+Math.PI = 5;
+
+console.log(Math.PI);
+
+const descriptor = Object.getOwnPropertyDescriptor(Math, "PI");
+console.log(descriptor);
+
+// create an object
+
+const chai = {
+    chaiName: "Ginger chai",
+    price: 20,
+    isAvailable: true,
+
+    orderChai: function(){
+        console.log("chai nahi bani");
+    }
+};
+
+console.log(chai);
+
+/* console.log(Object.getOwnPropertyDescriptor(chai, "chaiName"));
+
+// Enabling the object to update or change it's properties
+
+Object.defineProperty(chai, "chaiName", {
+    writable: false,
+    enumerable: false
+});
+
+console.log(Object.getOwnPropertyDescriptor(chai, "chaiName")); */
+
+for (let [key, value] of Object.entries(chai)) {
+    if (typeof value !== "function") {
+        console.log(`${key} : ${value}`);
+    }  
+}
